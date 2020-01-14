5b6be6e8cf9076d6e687a815920f2f9bc56512f5
ChelCod <vladislavbezverhnii@gmail.com>
2020-01-14 23:16:45 +0300

    update header, add scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import styles from './Header.module.css'
+import styles from './Header.module.scss'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import {NavLink} from 'react-router-dom'
 
 
 const Header = () => {
     return <header className={styles.header}>
         <AppBar position="static">
         <Toolbar>
-          
+          <NavLink to='/'><span className={styles.homeLink}>MovieSearcher</span></NavLink>
         </Toolbar>
       </AppBar>
     </header>
