be2bfa3fc3107bbc770a744858620e80487bd55d
ChelCod <vladislavbezverhnii@gmail.com>
2020-02-16 23:24:38 +0300

    add getImagesMovie api

--- src/api/TMDbAPI.jsx
+++ src/api/TMDbAPI.jsx
@@ -5,13 +5,6 @@ const baseURL = 'https://api.themoviedb.org/3/'
 
 
 
-
-
-// export const getPopularMovies = (page=1, language='en-US') => {
-//     return axios.get(`${baseURL}movie/popular?api_key=${apiKey}&language=${language}&page=${page}`).then(res => res)
-
-// }
-
 export const getMovieDiscover = (page=1, language = 'en-US', { sort_by, asc, year, vote_average, with_genres }) => {
     const sort = sort_by + (asc ? '.asc' : '.desc')
     const release = year.trim()
@@ -27,14 +20,9 @@ export const getMovieDiscover = (page=1, language = 'en-US', { sort_by, asc, yea
             sort_by: sort,
         }
     }).then(res => res)
-
-
-    //     &language=${language}
-    // &sortBy=${sort}
-    // &page=${page}
-    // ${release.length > 0 ? '&year=' + release : ''}
-    // ${rating.length > 0 ? '&vote_average=' + rating : ''}
-    // ${genres.length > 0 ? '&with_genres=' + genres : ''}
+}
+export const getImagesMovie = (movieId) =>{
+    return axios.get(`${baseURL}movie/${movieId}/images?api_key=${apiKey}`).then(res => res)
 }
 
 export const getMovie = (movieId, language = 'en-US') => {
